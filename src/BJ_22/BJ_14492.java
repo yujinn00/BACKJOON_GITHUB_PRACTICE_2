483049ee7f8f42d60a6a86aa70246be5362f3abd
yujin <qrt365@naver.com>
2023-01-10 13:06:35 +0900

    22

--- src/BJ_22/BJ_14492.java
+++ src/BJ_22/BJ_14492.java
@@ -1,3 +1,63 @@
+//package BJ_22;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.StringTokenizer;
+//
+//public class BJ_14492 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//
+//        int n = Integer.parseInt(br.readLine()); // 행렬 크기 입력
+//        int[][] a = new int[n][n]; // 첫 번째 행렬 선언
+//        int[][] b = new int[n][n]; // 두 번째 행렬 선언
+//        int[][] c = new int[n][n]; // 부울곱된 행렬 선언
+//        int count = 0; // 1의 개수를 0으로 초기화
+//
+//        for (int i = 0; i < n; i++) {
+//            st = new StringTokenizer(br.readLine());
+//
+//            for (int j = 0; j < n; j++) {
+//                a[i][j] = Integer.parseInt(st.nextToken()); // 첫 번째 행렬 입력
+//            }
+//        }
+//
+//        for (int i = 0; i < n; i++) {
+//            st = new StringTokenizer(br.readLine());
+//
+//            for (int j = 0; j < n; j++) {
+//                b[i][j] = Integer.parseInt(st.nextToken()); // 두 번째 행렬 입력
+//            }
+//        }
+//
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                int sum = 0;
+//
+//                for (int k = 0; k < n; k++) {
+//                    sum += a[i][k] * b[k][j];
+//
+//                    if (sum == 1) {
+//                        c[i][j] = 1;
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if (c[i][j] == 1) {
+//                    count++;
+//                }
+//            }
+//        }
+//        System.out.println(count);
+//    }
+//}
+
 package BJ_22;
 
 import java.io.BufferedReader;
@@ -10,50 +70,41 @@ public class BJ_14492 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int a = Integer.parseInt(br.readLine()); // 행렬 크기 입력
-        int[][] b = new int[a][a]; // 첫 번째 행렬 선언
-        int[][] c = new int[a][a]; // 두 번째 행력 선언
-        int[][] d = new int[a][a]; // 부울곱된 행렬 선언
+        int n = Integer.parseInt(br.readLine()); // 행렬 크기 입력
+        int[][] a = new int[n][n]; // 첫 번째 행렬 선언
+        int[][] b = new int[n][n]; // 두 번째 행렬 선언
+        int[][] c = new int[n][n]; // 부울곱된 행렬 선언
         int count = 0; // 1의 개수를 0으로 초기화
 
-        for (int i = 0; i < a; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
 
-            for (int j = 0; j < a; j++) {
-                b[i][j] = Integer.parseInt(st.nextToken()); // 첫 번째 행렬 입력
+            for (int j = 0; j < n; j++) {
+                a[i][j] = Integer.parseInt(st.nextToken()); // 첫 번째 행렬 입력
             }
         }
 
-        for (int i = 0; i < a; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
 
-            for (int j = 0; j < a; j++) {
-                c[i][j] = Integer.parseInt(st.nextToken()); // 두 번째 행렬 입력
+            for (int j = 0; j < n; j++) {
+                b[i][j] = Integer.parseInt(st.nextToken()); // 두 번째 행렬 입력
             }
         }
 
-        for (int i = 0; i < a; i++) {
-            for (int j = 0; j < a; j++) {
-                int sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    c[i][j] += a[i][k] * b[k][j]; // 부울곱
 
-                for (int k = 0; k < a; k++) {
-                    sum += b[i][k] * c[k][j];
-
-                    if (sum == 1) {
-                        d[i][j] = 1;
+                    if (c[i][j] == 1) {
+                        count++;
                         break;
                     }
                 }
             }
         }
 
-        for (int i = 0; i < a; i++) {
-            for (int j = 0; j < a; j++) {
-                if (d[i][j] == 1) {
-                    count++;
-                }
-            }
-        }
         System.out.println(count);
     }
 }
--- src/BJ_22/BJ_18870.java
+++ src/BJ_22/BJ_18870.java
@@ -14,9 +14,9 @@ public class BJ_18870 {
         StringBuilder sb = new StringBuilder();
 
         int a = Integer.parseInt(br.readLine());
-        int[] b = new int[a]; // 입력 정수 배열
+        int[] b = new int[a]; // 입출력 정수 배열
         int[] c = new int[a]; // 정렬 정수 배열
-        int d = 0; // Value 값
+        int d = 0; // 좌표 압축 값
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -42,46 +42,46 @@ public class BJ_18870 {
     }
 }
 
-// // for-each 사용 O
-// package BJ_22;
-
-// import java.io.BufferedReader;
-// import java.io.IOException;
-// import java.io.InputStreamReader;
-// import java.util.Arrays;
-// import java.util.HashMap;
-// import java.util.StringTokenizer;
-
-// public class BJ_18870 {
-//     public static void main(String[] args) throws IOException {
-//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//         StringBuilder sb = new StringBuilder();
-
-//         int a = Integer.parseInt(br.readLine());
-//         int[] b = new int[a]; // 입력 정수 배열
-//         int[] c = new int[a]; // 정렬 정수 배열
-//         int d = 0; // Value 값
-
-//         StringTokenizer st = new StringTokenizer(br.readLine());
-
-//         for (int i = 0; i < a; i++) {
-//             b[i] = c[i] = Integer.parseInt(st.nextToken());
-//         }
-
-//         Arrays.sort(c);
-
-//         HashMap <Integer, Integer> map = new HashMap<>(); // 정수형 HashMap 선언
-
-//         for (int e : c) {
-//             if(!map.containsKey(e)) {
-//                 map.put(e, d);
-//                 d++;
-//             }
-//         }
-
-//         for (int f : b) {
-//             sb.append(map.get(f)).append(" ");
-//         }
-//         System.out.println(sb);
-//     }
-// }
+//// for-each 사용 O
+//package BJ_22;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.StringTokenizer;
+//
+//public class BJ_18870 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//
+//        int a = Integer.parseInt(br.readLine());
+//        int[] b = new int[a];
+//        int[] c = new int[a];
+//        int d = 0;
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        for (int i = 0; i < a; i++) {
+//            b[i] = c[i] = Integer.parseInt(st.nextToken());
+//        }
+//
+//        Arrays.sort(c);
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//
+//        for (int e : c) {
+//            if (!map.containsKey(e)) {
+//                map.put(e, d);
+//                d++;
+//            }
+//        }
+//
+//        for (int f : b) {
+//            sb.append(map.get(f)).append(" ");
+//        }
+//
+//        System.out.println(sb);
+//    }
+//}
