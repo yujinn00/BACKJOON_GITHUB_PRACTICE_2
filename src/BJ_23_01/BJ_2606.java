8bb69243d3f5c1bcb115dcdba335001bfd297d96
yujin <qrt365@naver.com>
2023-01-13 23:59:14 +0900

    22

--- src/BJ_23_01/BJ_2606.java
+++ src/BJ_23_01/BJ_2606.java
@@ -6,40 +6,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BJ_2606 {
-    static int a, b, start = 1, count = -1;
-    static boolean[][] graph;
-    static boolean[] visited;
-
-    public static void dfs(int k) {
-        visited[k] = true;
-        count++;
-        for (int i = 1; i <= a; i++) {
-            if (graph[k][i] && !visited[i]) {
-                dfs(i);
-            }
-        }
-    }
+    public static int v, e, start = 1, count = 0;
+    public static boolean[] vertex;
+    public static boolean[][] edge;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        a = Integer.parseInt(br.readLine()); // 컴퓨터의 수
-        b = Integer.parseInt(br.readLine()); // 컴퓨터 쌍의 수
+        v = Integer.parseInt(br.readLine()); // 컴퓨터 개수 (정점)
+        e = Integer.parseInt(br.readLine()); // 컴퓨터 쌍 개수 (간선)
 
-        graph = new boolean[a + 1][a + 1]; // 쌍으로 연결된 컴퓨터를 체크하기 위한 2차원 배열
-        visited = new boolean[a + 1]; // 감염된 컴퓨터를 체크하기 위한 1차원 배열
+        vertex = new boolean[v + 1]; // 감염된 컴퓨터를 체크하기 위한 1차원 배열
+        edge = new boolean[v + 1][v + 1]; // 쌍으로 연결된 컴퓨터를 체크하기 위한 2차원 배열
 
-        for (int i = 0; i < b; i++) {
+        for (int i = 0; i < e; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int c = Integer.parseInt(st.nextToken()); // 연결 컴퓨터 1
-            int d = Integer.parseInt(st.nextToken()); // 연결 컴퓨터 2
+            int x = Integer.parseInt(st.nextToken()); // 연결 컴퓨터 1
+            int y = Integer.parseInt(st.nextToken()); // 연결 컴퓨터 2
 
-            graph[c][d] = true; // 감염 처리
-            graph[d][c] = true; // 감염 처리
+            edge[x][y] = true; // 연결 처리
+            edge[y][x] = true; // 연결 처리
         }
 
         dfs(start);
         System.out.println(count);
+    }
+
+    public static void dfs(int k) { // 깊이 우선 탐색 // DFS, BFS 중 아무거나 활용해도 됨
+        vertex[k] = true; // 감염 처리
+        for (int i = 1; i <= v; i++) { // 1번 컴퓨터부터 컴퓨터 개수만큼 반복
+            if (edge[k][i] && !vertex[i]) { // 연결되어 있고, 감염되지 않은 컴퓨터이면 실행
+                count++; // 감염 수 증가
+                dfs(i); // 재귀 함수 dfs() 호출
+            }
+        }
     }
 }
