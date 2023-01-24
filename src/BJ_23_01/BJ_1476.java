6fdb5b6e2f44f7f247078711e90af190ecd1aa00
yujin <qrt365@naver.com>
2023-01-24 22:52:08 +0900

    23.01.24

---
+++ src/BJ_23_01/BJ_1476.java
@@ -0,0 +1,39 @@
+package BJ_23_01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_1476 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int E = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int e = 0; int s = 0; int m = 0; int year = 0;
+
+        while (true) {
+            e++; s++; m++; year++;
+
+            if (e > 15) {
+                e = 1;
+            }
+
+            if (s > 28) {
+                s = 1;
+            }
+
+            if (m > 19) {
+                m = 1;
+            }
+
+            if (E == e && S == s && M == m) {
+                System.out.println(year);
+                break;
+            }
+        }
+    }
+}
